8069cd8c771ce780e3b18d86edaf0e984ec7cc19
Proxy Mallet <qq84063@gmail.com>
2022-12-02 00:00:46 +0500

    Update genwordAnimeGenerator.py

--- genwordAnimeGenerator.py
+++ genwordAnimeGenerator.py
@@ -1,15 +1,19 @@
-class animeGenerator:
+class genwordGenerator:
     def __init__(self):
         self.Session = __import__('requests').Session()
-        self.url = 'https://genword.ru/generators/anime/{}'.format
+        self.url = 'https://genword.ru/generators/{}/new/'.format
         self.headers = {
             'user-agent': __import__('fake_useragent').UserAgent()['Opera'],
             'x-requested-with': 'XMLHttpRequest'
         }
 
-    def generate(self):
-        req = self.Session.post(self.url('new/'), headers = self.headers).json()['result']
+    def Anime(self):
+        req = self.Session.post(self.url('anime'), headers = self.headers).json()['result']
         if input('1 - Eng\n2 - Rus\n>>> ') == '1': return req['nameEn']
         else: return req['nameRu']
 
-print(animeGenerator().generate())
+    def words(self):
+        req = self.Session.post(self.url('word'), headers=self.headers).json()['result']
+        return req['word']
+
+print(genwordGenerator().words())
