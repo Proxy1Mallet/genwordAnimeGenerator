f71ac0add00c4fe1935b628f5894caca86af38f1
Proxy Mallet <qq84063@gmail.com>
2022-12-02 12:20:19 +0500

    Update genwordGenerator.py

--- genwordGenerator.py
+++ genwordGenerator.py
@@ -8,32 +8,32 @@ class genwordGenerator:
         self.headers = headers.Headers().headers
 
     def anime(self):
-        req = self.Session.post(self.url('anime'), headers = self.headers)
+        req = self.Session.post(url = self.url('anime'), headers = self.headers)
         return objects.Anime(req.json()).Anime
 
     def words(self):
-        req = self.Session.post(self.url('word'), headers=self.headers)
+        req = self.Session.post(url = self.url('word'), headers = self.headers)
         return objects.Words(req.json()).Words
 
     def winged(self):
-        req = self.Session.post(self.url('winged'), headers=self.headers)
+        req = self.Session.post(url = self.url('winged'), headers = self.headers)
         return objects.Winged(req.json()).Winged
 
     def alcoholDrinking(self):
-        req = self.Session.post(self.url('alcohol-drinking'), headers=self.headers)
+        req = self.Session.post(url = self.url('alcohol-drinking'), headers = self.headers)
         return objects.AlcoholDrinking(req.json()).AlcoholDrinking
 
     def alias(self, alias, sex):
         data = {'alias': alias, 'sex': sex}
-        req = self.Session.post(self.url('alias'), headers = self.headers, data = data)
+        req = self.Session.post(url = self.url('alias'), headers = self.headers, data = data)
         return objects.Alias(req.json()).Alias
 
     def slogan(self, slogan):
-        data = {slogan: slogan}
-        req = self.Session.post(self.url('slogan'), headers=self.headers, data=data)
+        data = {'slogan': slogan}
+        req = self.Session.post(url = self.url('slogan'), headers = self.headers, data = data)
         return objects.Slogan(req.json()).Slogan
 
     def login(self, firstname, surname, patronymic, nickname):
         data = {'firstname': firstname, 'surname': surname, 'patronymic': patronymic, 'nickname': nickname}
-        req = self.Session.post(self.url('login'), headers=self.headers, data=data)
+        req = self.Session.post(url = self.url('login'), headers = self.headers, data = data)
         return objects.Login(req.json()).Login
