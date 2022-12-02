bce3cb6b39b3f90186b67a50d0fe4bbdf6c12ded
Proxy Mallet <qq84063@gmail.com>
2022-12-02 12:31:08 +0500

    Update headers.py

--- util/headers.py
+++ util/headers.py
@@ -3,6 +3,6 @@ from fake_useragent import UserAgent
 class Headers:
     def __init__(self):
         self.headers = {
-            'user-agent': UserAgent()['opera'],
+            'user-agent': UserAgent().random,
             'x-requested-with': 'XMLHttpRequest'
         }
