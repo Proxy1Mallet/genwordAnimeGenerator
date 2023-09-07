5608ea920e447bf3f13a8c295416d9dfca85d57c
Proxy Mallet <qq84063@gmail.com>
2023-09-07 20:31:41 +0500

    minor changes

--- genword_API.py
+++ genword_API.py
@@ -1,45 +1,39 @@
-from .objects import *
 from requests import Session
 from fake_useragent import FakeUserAgent
+
+from .objects import *
+
 class Data:
     _session = Session()
     _url = 'https://genword.ru/generators/{}/new/'.format
     _headers = {
         'user-agent': FakeUserAgent().random,
-        'accept-encoding': 'gzip, deflate, br',
-        'accept': 'application/json, text/javascript, */*; q=0.01',
         'x-requested-with': 'XMLHttpRequest'
     }
     _data = {
-        'x-csrf-token': _session.get(url = 'https://genword.ru/', headers = _headers).text[6901:][:64],
-        'x-powered-by': 'PHP/7.2.34',
-        'vary': 'Accept-Encoding'
+        'x-csrf-token': _session.get(url = 'https://genword.ru/', headers = _headers).headers['X-CSRF-Token']
     }
 
 class GenwordAPI(Data):
     @classmethod
     def anime(cls) -> int | ObjectAnime:
-        req = cls._session.post(url = cls._url('anime'), data = cls._data, headers = cls._headers)
-        if req.status_code != 200: return req.status_code
-        else: return ObjectAnime(data = req.json()).object_anime
+        _req = cls._session.post(url = cls._url('anime'), data = cls._data, headers = cls._headers)
+        return _req.status_code if _req.status_code != 200 else ObjectAnime(data = _req.json()).object_anime
 
     @classmethod
     def words(cls) -> int | ObjectWords:
-        req = cls._session.post(url = cls._url('word'), data = cls._data, headers = cls._headers)
-        if req.status_code != 200: return req.status_code
-        else: return ObjectWords(data = req.json()).object_words
+        _req = cls._session.post(url = cls._url('word'), data = cls._data, headers = cls._headers)
+        return _req.status_code if _req.status_code != 200 else ObjectWords(data = _req.json()).object_words
 
     @classmethod
     def winged(cls) -> int | ObjectWinged:
-        req = cls._session.post(url = cls._url('winged'), data = cls._data, headers = cls._headers)
-        if req.status_code != 200: return req.status_code
-        else: return ObjectWinged(data = req.json()).object_winged
+        _req = cls._session.post(url = cls._url('winged'), data = cls._data, headers = cls._headers)
+        return _req.status_code if _req.status_code != 200 else ObjectWinged(data = _req.json()).object_winged
 
     @classmethod
     def alcohol_drinking(cls) -> int | ObjectAlcoholDrinking:
-        req = cls._session.post(url = cls._url('alcohol-drinking'), data = cls._data, headers = cls._headers)
-        if req.status_code != 200: return  req.status_code
-        else: return ObjectAlcoholDrinking(data = req.json()).object_alcohol_drinking
+        _req = cls._session.post(url = cls._url('alcohol-drinking'), data = cls._data, headers = cls._headers)
+        return _req.status_code if _req.status_code != 200 else ObjectAlcoholDrinking(data = _req.json()).object_alcohol_drinking
 
     @classmethod
     def alias(cls, animal : str, sex : int) -> int | ObjectAlias:
@@ -55,9 +49,8 @@ class GenwordAPI(Data):
         :return: an alias for an animal in json format
         """
         cls._data.update(alias = animal, sex = sex)
-        req = cls._session.post(url = cls._url('alias'), headers = cls._headers, data = cls._data)
-        if req.status_code != 200: return req.status_code
-        else: return ObjectAlias(data = req.json()).object_alias
+        _req = cls._session.post(url = cls._url('alias'), headers = cls._headers, data = cls._data)
+        return _req.status_code if _req.status_code != 200 else ObjectAlias(data = _req.json()).object_alias
 
     @classmethod
     def slogan(cls, slogan : str) -> int | ObjectSlogan:
@@ -70,9 +63,8 @@ class GenwordAPI(Data):
         :return: Slogan in json format
         """
         cls._data.update(slogan = slogan)
-        req = cls._session.post(url = cls._url('slogan'), headers = cls._headers, data = cls._data)
-        if req.status_code != 200: return req.status_code
-        else: return ObjectSlogan(data = req.json()).object_slogan
+        _req = cls._session.post(url = cls._url('slogan'), headers = cls._headers, data = cls._data)
+        return _req.status_code if _req.status_code != 200 else ObjectSlogan(data = _req.json()).object_slogan
 
     @classmethod
     def login(cls, firstname : str, surname : str, patronymic : str, nickname : str) -> int | ObjectLogin:
@@ -94,6 +86,5 @@ class GenwordAPI(Data):
         :return: 10 logins in json format
         """
         cls._data.update(firstname = firstname, surname = surname, patronymic = patronymic, nickname = nickname)
-        req = cls._session.post(url = cls._url('login'), headers = cls._headers, data = cls._data)
-        if req.status_code != 200: return req.status_code
-        else: return ObjectLogin(data = req.json()).object_login
+        _req = cls._session.post(url = cls._url('login'), headers = cls._headers, data = cls._data)
+        return _req.status_code if _req.status_code != 200 else ObjectLogin(data = _req.json()).object_login
